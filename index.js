c57dbc326d7ae0ba29532f16a5fdf10be8e182df
翟宇佳 <bjzhaiyujia@corp.netease.com>
2020-03-29 16:31:18 +0100

    测试base64图片绘制，img下载

--- index.js
+++ index.js
@@ -1,75 +1,138 @@
-let image;
+let image = new Image();
 function upload() {
-    let bg = new Image();
-    bg.src = "./img/bg"
-
-    let canvas = document.querySelector("#resultCanvas"); 
-    let ctx = canvas.getContext("2d");
-    ctx.drawImage(bg, 0, 0, 300, 500)
+    // ctx.drawImage(bg, 0, 0, 300, 500)
 
 	let file = document.querySelector('input[type=file]').files[0]  // 获取选择的文件，这里是图片类型
 	let reader = new FileReader()
         reader.readAsDataURL(file) //读取文件并将文件以URL的形式保存在resulr属性中 base64格式
         reader.onload = function(e) { // 文件读取完成时触发 
             let result = e.target.result // base64格式图片地址 
-            image = new Image();
+            image.crossOrigin = '';
             image.src = result // 设置image的地址为base64的地址 
-            image.onload = function(){ 
-                // let canvas = document.querySelector("#resultCanvas"); 
-                // let ctx = canvas.getContext("2d");
-                // // ctx.clearRect(0, 0, 700, 1000);
-                // ctx.drawImage(bg, 0, 0, 300, 500)
-                // // canvas.width = image.width; // 设置canvas的画布宽度为图片宽度 
-                // // canvas.height = image.height; 
-                // // context.drawImage(image, 0, 0, image.width, image.height) // 在canvas上绘制图片 
-                // let dataUrl = canvas.toDataURL('image/jpeg', 0.92) // 0.92为压缩比，可根据需要设置，设置过小会影响图片质量 
-                                                                   // dataUrl 为压缩后的图片资源，可将其上传到服务器 
-            } 
+            
        }
  }
+ function makeImg(canvas) {
+    let img = document.getElementById("save_img");
+    let tempSrc = canvas.toDataURL("image/png");
+    img.src = tempSrc;
+    console.log(tempSrc)
+  }
+  function createPic(){
+    let canvas = document.getElementById("resultCanvas"); 
+    // if(canvas.getContext){
+        let img = document.getElementById("save_img");
+        img.setAttribute("crossOrigin",'Anonymous')
+        let tempSrc = canvas.toDataURL("image/png");
+        img.src = tempSrc;
+    // }
+    console.log(tempSrc)
+  }
+  
  function paintBg(){
     let bg = new Image();
     let btn = new Image();
     let left = new Image();
     let right = new Image();
+    let img = document.getElementById("save_img");
     let name = document.getElementById("name").value;
     let school = document.getElementById("school").value;
-    let nation = document.getElementById("nation").value;
+    let second = document.getElementById("second").value||"";
     let degree = document.getElementById("degree").value;
     let rant = document.getElementById("rant").value;
+    let canvas = document.getElementById("resultCanvas"); 
+    let ctx = canvas.getContext("2d");
     bg.src = "./img/bg.png";
     left.src = "./img/left.png";
     right.src = "./img/right.png";
-    btn.src = "./img/btn.png";
-    if(name&&school&&nation&&degree&&rant){
+    btn.src = "./img/btn.png";  
+    // if(name&&school&&degree&&rant){
         bg.onload = function(){
-            let canvas = document.querySelector("#resultCanvas"); 
-            let ctx = canvas.getContext("2d");
             ctx.clearRect(0,0,1080,1920);
             ctx.drawImage(bg, 0, 0, 1080, 1920);
             ctx.drawImage(btn, 299, 525);
-            ctx.drawImage(image,0,0,image.width,650,135,1000,800,650);      
-            // ctx.drawImage(bg, 0, 0, 300, 520);
+            ctx.drawImage(image,0,0,image.width,650,135,970,800,650);    
+            ctx.fillStyle="#000000";
             ctx.font = 'bold 57px Adobe Ming Std';
             ctx.fillText(`2020录取捷报`, 358,603);
             ctx.font = 'bold 50px Adobe Ming Std';
-            ctx.fillText(`益文${name}同学`, 125, 773)
-            ctx.fillText(`${school}`, 730, 773)
+            ctx.fillText(`益文${name}同学`, 125, 743)
+            ctx.fillText(`${school}`, 730, 743)
             ctx.font = 'bold 40px Adobe Ming Std';
-            ctx.fillText(`背景: ${nation}`, 125, 873)
-            ctx.fillText(`2020 QS`, 480, 873)
-            ctx.fillText(`世界大学排名`, 480, 923)
-            ctx.fillText(degree, 125, 923)
+            ctx.fillText(`背景: ${degree}`, 125, 843)
+            ctx.fillText(`2020 QS`, 480, 843)
+            ctx.fillText(`世界大学排名`, 480, 893)
+            ctx.fillText(second, 125, 893)
             ctx.font = 'bold 70px Adobe Ming Std';
             ctx.fillStyle="#B61D1C";
-            ctx.fillText(rant, 797, 903)
+            ctx.fillText(rant, 797, 873)
             ctx.font = 'bold 50px Adobe Ming Std';
-            ctx.fillText(`录取学校: `, 480, 773)
-            ctx.drawImage(left, 66, 1360);
+            ctx.fillText(`录取学校: `, 480, 743)
+            ctx.drawImage(left, 66, 1500);
             ctx.drawImage(right, 268, 1500);
+            console.log(image)
+            let tempSrc = canvas.toDataURL("image/png");
+            img.src = tempSrc;
         }
-    }else{
-        alert("填写信息不完整")
-    }
-    
+    // }else{
+    //     alert("填写信息不完整")
+    // }
  }
+
+
+
+ function loadImage(src, callback) {
+    var img = new Image();
+
+    img.onload = callback;
+    img.setAttribute('crossorigin', 'anonymous'); // works for me
+
+    img.src = src;
+
+    return img;
+}
+function doInput(id){
+    var inputObj = document.createElement('input');
+    inputObj.addEventListener('change',readFile,false);
+    inputObj.type = 'file';
+    inputObj.accept = 'image/*';
+    inputObj.id = id;
+    inputObj.click();
+}
+function readFile(){
+    var file = this.files[0];//获取input输入的图片
+    if(!/image\/\w+/.test(file.type)){
+        alert("请确保文件为图像类型");
+        return false;
+    }//判断是否图片，在移动端由于浏览器对调用file类型处理不同，虽然加了accept = 'image/*'，但是还要再次判断
+    var reader = new FileReader();
+    reader.readAsDataURL(file);//转化成base64数据类型
+    reader.onload = function(e){
+            drawToCanvas(this.result);
+        }
+}
+
+function drawToCanvas(imgData){
+    var cvs = document.querySelector('#resultCanvas');
+        cvs.width=300;
+        cvs.height=400;
+        var ctx = cvs.getContext('2d');
+        let showImage = document.getElementById("save_img");
+        var img = new Image;
+            img.src = imgData;
+            // img.onload = function(){//必须onload之后再画
+            //     ctx.drawImage(image,0,0,image.width,650,135,970,800,650); 
+            //     // let tempSrc = canvas.toDataURL("image/png");
+            //     setTimeout(() => {   
+            //         strDataURI = cvs.toDataURL();//获取canvas base64数据
+            //         showImage.src = strDataURI;
+            //     }, 1000);
+            // }
+            img.addEventListener('load' , function(){
+                ctx.drawImage(image,0,0,image.width,650,135,970,800,650); 
+                setTimeout(() => {   
+                    strDataURI = cvs.toDataURL();//获取canvas base64数据
+                    showImage.src = strDataURI;
+                }, 1000);
+              } , false);
+}
